e7323ffd6c3386d027c2ad5d9bc931c93b96b145
rjainsobha-debug <rjain.sobha@gmail.com>
2026-04-02 16:02:57 +0530

    Update apis-config.js

--- js/apis-config.js
+++ js/apis-config.js
@@ -1,3 +1,3 @@
 // Paste your metals.dev API key below to enable live gold and silver prices.
 // Example: export const METALS_API_KEY = "your_key_here";
-export const METALS_API_KEY = "";
+export const METALS_API_KEY = "CODKM60Y03YEVRZOGLWQ641ZOGLWQ";
