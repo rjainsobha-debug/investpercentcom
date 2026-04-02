dedd2439de2983f6a93b1f54b0edd13a3b87cbf0
rjainsobha-debug <rjain.sobha@gmail.com>
2026-04-02 15:10:12 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,12 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
+  apiKey: "AIzaSyB11Do3r1p59ITDCsz_kgxf5xAdozGE2WY",
   authDomain: "investpercent.firebaseapp.com",
   projectId: "investpercent",
-  storageBucket: "investpercent.appspot.com",
-  messagingSenderId: "PASTE",
-  appId: "PASTE"
+  storageBucket: "investpercent.firebasestorage.app",
+  messagingSenderId: "1004946276963",
+  appId: "1:1004946276963:web:dfe950c029f24c4819ee56",
+  measurementId: "G-1V4L60RZZP"
 };
 
 const app = initializeApp(firebaseConfig);
