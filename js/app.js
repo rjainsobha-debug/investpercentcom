02e33e3dcaf874dad5bc72a5d3224ff4c02abd42
rjainsobha-debug <rjain.sobha@gmail.com>
2026-04-02 14:46:40 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,7 @@
-// popup
-function showPopup(){
-  document.getElementById("leadPopup").style.display="flex";
-}
-function submitLead(){
-  window.open("https://wa.me/918882332050");
+function openPopup(){
+  document.getElementById("popup").style.display="flex";
 }
 
-// market simulation
-setInterval(()=>{
-  document.querySelector(".ticker").innerHTML = `
-    NIFTY: ${22000+Math.floor(Math.random()*200)} |
-    SENSEX: ${73000+Math.floor(Math.random()*300)} |
-    GOLD: ${72000+Math.floor(Math.random()*200)} |
-    SILVER: ${85000+Math.floor(Math.random()*300)}
-  `;
-},2000);
+function goToWhatsApp(){
+  window.open("https://wa.me/918882332050");
+}
