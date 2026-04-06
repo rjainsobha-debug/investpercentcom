490c37b28945442994f85cb7b69cb4611e4c2690
rjainsobha-debug <rjain.sobha@gmail.com>
2026-04-06 11:31:40 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+function loadNavbar() {
+  fetch('navbar.html')
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById('navbar').innerHTML = data;
+      setActiveLink();
+    });
+}
+
+function setActiveLink() {
+  const links = document.querySelectorAll('.nav-link');
+  const currentPage = window.location.pathname.split("/").pop() || "index.html";
+
+  links.forEach(link => {
+    const linkPage = link.getAttribute("href");
+    if (linkPage === currentPage) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+}
+
+function toggleMenu() {
+  const drawer = document.getElementById('mobileDrawer');
+  if (drawer) drawer.classList.toggle('open');
+}
+
 function loadComponent(id, file) {
   fetch(file)
     .then(res => res.text())
@@ -5,3 +33,5 @@ function loadComponent(id, file) {
       document.getElementById(id).innerHTML = data;
     });
 }
+
+document.addEventListener("DOMContentLoaded", loadNavbar);
